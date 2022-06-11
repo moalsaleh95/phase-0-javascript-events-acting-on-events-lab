24354ad5a8f5b1b95d1176d4a1775e784c172920
Mohammad Al Saleh <mo.alsaleh95@gmail.com>
2022-06-11 17:32:33 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+dodger.style.background = "red";
+
+function moveDodgerLeft() {
+    const left = parseInt(dodger.style.left, 10)
+
+    if (left > 0) {
+        dodger.style.left = `${left - 5}px`;
+    }
+}
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+});
+
+function moveDodgerRight() {
+    const left = parseInt(dodger.style.left);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 5}px`;
+    }
+}
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+})
